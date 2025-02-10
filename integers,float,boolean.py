16c4fc96871dfc9af251683cd89385c2441bc28f
moyanshpanwar <moyanshpanwar1911@gmail.com>
2025-02-10 18:06:16 +0530

    Add files via upload

---
+++ integers,float,boolean.py
@@ -0,0 +1,58 @@
+#basic arithematic
+print(2+1)     #addition
+print(2-5)     #subtraction
+print(2*2)     #multiplication
+print(3/2)     #division
+print(3//2)    #floor division
+print(3%2)     #modulus
+print(3**2)     #exponentation
+
+#order followed in python
+print(2+10*10+3)
+print(2+10*(10+3))
+print()
+
+#variables
+x=5
+print(x)
+y=6.4
+print(y)
+
+#operations using variables
+z=x+y           #addition
+print(z)
+z=x-y           #subtraction
+print(z)
+print(type(z))
+z=x*y           #multiplication
+print(z)
+print(type(z))
+z=x/y           #division
+print(z)
+print(type(z))
+z=x//y          #floor division
+print(z)
+
+#boolean variables
+#equal
+print(2==3)
+print(2==0)
+#not equal
+print(2!=0)
+print(2!=2)
+#greater than
+a=3
+b=2
+print(a>b)
+print(a==3)
+print(b>4)
+#less than
+print(10<45)
+print(4<2)
+#greater than or equal to
+print(3>=2)
+print(4>=4)
+#less than or equal to
+print(3<=0)
+print(1<=2)
+
